2ccdc48f676218090cde6b92d07bda931da0cfa1
mehran <mehran.naseri2013@gmail.com>
2022-04-27 10:33:11 +0430

    retrive all links for user

--- Controllers/LinkController.php
+++ Controllers/LinkController.php
@@ -6,6 +6,7 @@ use App\Core\Application;
 use App\Classes\LinkShortener;
 use App\Core\Middlewares\AuthMiddleware;
 use App\Core\Request;
+use App\Models\Links;
 
 class LinkController extends BaseController
 {
@@ -17,7 +18,22 @@ class LinkController extends BaseController
     public function index()
     {
         $user = Application::$app->auth;
-        dd($user);
+        $linkModel = new Links();
+        $links = $linkModel->getAll($user->id);
+
+        $base_url = Application::url();
+        $data = [];
+        foreach ($links as $link){
+            $data[] = [
+                'id' => $link['id'],
+                'short_url' => $base_url.$link['shortened_link'],
+                'redirect_to' => $link['main_address'],
+                'create_at' => $link['created_at'],
+                'expire_at' => $link['expire_at']
+            ];
+        }
+
+        return $this->response(200, "successful", ['links' => $data]);
     }
 
     public function store(Request $request)
--- Models/Links.php
+++ Models/Links.php
@@ -21,10 +21,19 @@ class Links extends Model
     {
         $query = "INSERT INTO $this->table (user_id, main_address, shortened_link, expire_at) VALUES (:user, :main_address, :url, :expire)";
         $sql = $this->connection->prepare($query);
-        $sql->bindParam("user" , $user_id);
-        $sql->bindParam("main_address" , $main_address);
-        $sql->bindParam("url" , $short_url);
-        $sql->bindParam("expire" , $expire_time);
+        $sql->bindValue(":user" , $user_id);
+        $sql->bindValue(":main_address" , $main_address);
+        $sql->bindValue(":url" , $short_url);
+        $sql->bindValue(":expire" , $expire_time);
         return $sql->execute();
+    }
+
+    public function getAll($user_id)
+    {
+        $query = "SELECT * FROM $this->table where user_id = :user_id";
+        $sql = $this->connection->prepare($query);
+        $sql->bindValue(":user_id", $user_id);
+        $sql->execute();
+        return $sql->fetchAll();
     }
 }
