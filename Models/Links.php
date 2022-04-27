64f721cf00656ab0bf711cd618d3c31e601a238c
mehran <mehran.naseri2013@gmail.com>
2022-04-27 10:19:03 +0430

    remove generate expire time on model and move it to link shortener class

--- Models/Links.php
+++ Models/Links.php
@@ -17,10 +17,8 @@ class Links extends Model
         return $sql->rowCount();
     }
 
-    public function saveLink($user_id, $main_address, $short_url, $timestamp)
+    public function saveLink($user_id, $main_address, $short_url, $expire_time)
     {
-        $expire_time = $this->expireTime($timestamp);
-
         $query = "INSERT INTO $this->table (user_id, main_address, shortened_link, expire_at) VALUES (:user, :main_address, :url, :expire)";
         $sql = $this->connection->prepare($query);
         $sql->bindParam("user" , $user_id);
@@ -28,11 +26,5 @@ class Links extends Model
         $sql->bindParam("url" , $short_url);
         $sql->bindParam("expire" , $expire_time);
         return $sql->execute();
-    }
-
-    public function expireTime($time)
-    {
-        $time = strtotime($time);
-        return date("Y-m-d H:i:s", strtotime("+1 month", $time));
     }
 }
